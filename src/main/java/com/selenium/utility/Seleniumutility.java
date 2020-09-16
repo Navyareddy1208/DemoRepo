dd24d4bf89b027ded9ca6df312434dc296de37f7
navya <navyareddyam@gmail.com>
2020-09-16 13:04:28 +0530

    this is my first selenium code

---
+++ src/main/java/com/selenium/utility/Seleniumutility.java
@@ -0,0 +1,10 @@
+package com.selenium.utility;
+
+public class Seleniumutility {
+
+	public static void testUtil() {
+	System.out.println("i am from util class");
+	
+	}
+
+}
---
+++ src/test/java/com/test/Testclass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Testclass {
+
+	@Test
+	public static void test()
+	{
+	 System.out.println("i am from test1 ");
+		
+	}
+}
